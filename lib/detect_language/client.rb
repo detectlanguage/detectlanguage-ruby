c098487c936bb5a002d0fa9659ebc6cd515d1323
Laurynas Butkus <laurynas.butkus@gmail.com>
2018-06-24 16:35:41 +0300

    Cast API key to string

--- lib/detect_language/client.rb
+++ lib/detect_language/client.rb
@@ -32,7 +32,7 @@ module DetectLanguage
         request.set_form_data(params)
       end
 
-      request.add_field('Authorization', 'Bearer ' + configuration.api_key)
+      request.add_field('Authorization', 'Bearer ' + configuration.api_key.to_s)
       request.add_field('User-Agent', configuration.user_agent)
 
       response = http.request(request)
